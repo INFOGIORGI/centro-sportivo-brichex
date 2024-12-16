d859b3cba7211e891278a781b540b42639c81447
brichex <brianromano51@gmail.com>
2024-12-16 22:55:15 +0000

    .

--- .vscode/Istruttore.java
+++ .vscode/Istruttore.java
@@ -1,12 +1,14 @@
+
 public class Istruttore {
 
     private String nome;
     private String cognome;
     private String ci;
-
-    public Istruttore(){
+    public Istruttore(String nome, String cognome, String ci) {
         this.nome = nome;
         this.cognome = cognome;
         this.ci = ci;
     }
+
+    
 }
