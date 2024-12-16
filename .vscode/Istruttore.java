2377f975c9ac5bcf92d335bad534c617ef85f4e4
brichex <brianromano51@gmail.com>
2024-12-16 22:54:42 +0000

    .

---
+++ .vscode/Istruttore.java
@@ -0,0 +1,12 @@
+public class Istruttore {
+
+    private String nome;
+    private String cognome;
+    private String ci;
+
+    public Istruttore(){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.ci = ci;
+    }
+}
---
+++ .vscode/Socio.java
@@ -0,0 +1,13 @@
+public class Socio {
+private String nome;
+private String cognome;
+private String cs;
+
+public Socio(){
+    this.nome = nome;
+    this.cognome = cognome;
+    this.cs = cs;
+
+}
+    
+}
